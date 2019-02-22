566bc3ac80ba248b5360873a94c998927ed18beb
Brad Orego <me@bradorego.com>
2019-02-22 14:06:23 -0500

    far too lazy to list all of the updates

--- static/fv.js
+++ static/fv.js
@@ -1,7 +1,7 @@
 let directionsService = new google.maps.DirectionsService;
 let zipCodesFlat = [10453,10457,10460,10458,10467,10468,10451,10452,10456,10454,10455,10459,10474,10463,10471,10466,10469,10470,10475,10461,10462,10464,10465,10472,10473,11212,11213,11216,11233,11238,11209,11214,11228,11204,11218,11219,11230,11234,11236,11239,11223,11224,11229,11235,11201,11205,11215,11217,11231,11203,11210,11225,11226,11207,11208,11211,11222,11220,11232,11206,11221,11237,10026,10027,10030,10037,10039,10001,10011,10018,10019,10020,10036,10029,10035,10010,10016,10017,10022,10012,10013,10014,10004,10005,10006,10007,10038,10280,10002,10003,10009,10021,10028,10044,10065,10075,10128,10023,10024,10025,10031,10032,10033,10034,10040,11361,11362,11363,11364,11354,11355,11356,11357,11358,11359,11360,11365,11366,11367,11412,11423,11432,11433,11434,11435,11436,11101,11102,11103,11104,11105,11106,11374,11375,11379,11385,11691,11692,11693,11694,11695,11697,11004,11005,11411,11413,11422,11426,11427,11428,11429,11414,11415,11416,11417,11418,11419,11420,11421,11368,11369,11370,11372,11373,11377,11378,10302,10303,10310,10306,10307,10308,10309,10312,10301,10304,10305,10314];
 const API_BASE = "";
-// let zipCodesFlat = [10453,10457,10460,10458,10467];
+// let zipCodesFlat = [10017,10457,10460,10458,10467];
 
 document.addEventListener('DOMContentLoaded', () => {
   let $driveTarget = $('#fv-input-drive'),
@@ -18,7 +18,8 @@ document.addEventListener('DOMContentLoaded', () => {
     $rentStatus = $('#rent-status')
     $formattedOutput = $('#formatted-output'),
     $formOutDriving = $('#formatted-output-driving'),
-    $formOutTransit = $('#formatted-output-transit');
+    $formOutTransit = $('#formatted-output-transit'),
+    $checkDriving = $('#check-driving');
 
   let driveAutocomplete = new google.maps.places.Autocomplete($driveTarget[0]);
   driveAutocomplete.addListener('place_changed', () => {
@@ -57,14 +58,14 @@ document.addEventListener('DOMContentLoaded', () => {
   $form.submit((e) => {
     e.preventDefault();
     e.stopPropagation();
-    if (data.params.DRIVING.destination) {
-      if (data.params.TRANSIT.destination) {
-        methods.query();
+    if (data.params.TRANSIT.destination) {
+      if ($checkDriving.val() && !data.params.DRIVING.destination) {
+        alert("Please select driving destination");
       } else {
-        alert("Please select transit destination");
+        methods.query();
       }
     } else {
-      alert("Please select driving destination");
+      alert("Please select transit destination");
     }
     // methods.query();
   });
@@ -100,10 +101,16 @@ document.addEventListener('DOMContentLoaded', () => {
   };
   let methods = {
     stateReset: function () {
-      data.status.DRIVING = {resolved: 0, total: formattedZips.length};
-      data.status.TRANSIT = {resolved: 0, total: 0};
+      data.status.DRIVING = {resolved: 0, total: 0};
+      data.status.TRANSIT = {resolved: 0, total: formattedZips.length};
       data.status.rent = {resolved: 0, total: 0};
       data.outputZips = [];
+      $formOutTransit.empty();
+      $formOutDriving.empty();
+      $formattedOutput.empty();
+      $driveStatus.empty();
+      $transitStatus.empty();
+      $rentStatus.empty();
     },
     query: function () {
       methods.stateReset();
@@ -129,25 +136,28 @@ document.addEventListener('DOMContentLoaded', () => {
       $outputDrivingLabel.text(`${data.params.DRIVING.max / 60} minute drive to ${data.params.DRIVING.destination} at ${drivingTime.toLocaleString()}`);
       $outputTransitLabel.text(`${data.params.TRANSIT.max / 60} minute public transit to ${data.params.TRANSIT.destination} at ${transitTime.toLocaleString()}`);
 
-      methods.getDrivingDirections(formattedZips).then((drivingZips) => {
-        methods.formatOutput(drivingZips, $formOutDriving);
-        data.status.TRANSIT.total = drivingZips.length;
-        return methods.getTransitDirections(drivingZips);
-      }).then((transitZips) => {
+      methods.getTransitDirections(formattedZips).then((transitZips) => { /// let's do transit first
         methods.formatOutput(transitZips, $formOutTransit);
-        data.status.rent.total = transitZips.length;
-        return methods.getPrices(transitZips);
+        data.status.DRIVING.total = transitZips.length;
+        if ($checkDriving.val()) {
+          return methods.getDrivingDirections(transitZips);
+        }
+        $driveStatus.text('N/A');
+        return Promise.resolve(transitZips);
+      }).then((drivingZips) => {
+        methods.formatOutput(drivingZips, $formOutDriving);
+        data.status.rent.total = drivingZips.length;
+        return methods.getPrices(drivingZips);
       }).then((pricesZips) => {
         /// TODO get walkscore
+        pricesZips.sort((a, b) => a.code - b.code); /// sort them by zip code
         data.outputZips = pricesZips.splice();
         methods.formatOutput(pricesZips, $formattedOutput);
-      })
-      .catch((error) => {
+      }).catch((error) => {
         console.warn(error);
         alert(`There was an unexpected error. ${error}`);
-      })
-      .finally(() => {
-        /// show/hide divs
+      }).finally(() => {
+        /// clean up
       });
     },
     formatOutput: function (zips, jqDiv) {
@@ -198,12 +208,25 @@ document.addEventListener('DOMContentLoaded', () => {
     getTransitDirections: function (zips) {
       return methods.directionsAbstraction(zips, 'TRANSIT');
     },
+    getDirectionAsPromise: function (options) {
+      return new Promise((res, rej) => {
+        directionsService.route(options, (response, status) => {
+          if (status === 'OK') {
+            res(response);
+          } else {
+            rej({"response": response, "status": status}); ///useful on errors; not useful on success
+          }
+        })
+      });
+    },
     directionsAbstraction: function (zips, which) { /// zips = {code, TRANSIT, DRIVING}, which === "DRIVING"|"TRANSIT"
+      if (!zips.length) return new Promise((res, rej) => {res(zips)});
       return new Promise((res, rej) => {
         let promises = [];
         let index = 0;
         let outputZips = [];
-        let findFastestRoute = (routes) => {
+        let invocations = 0;
+        const findFastestRoute = (routes) => {
           let minDur = Number.MAX_SAFE_INTEGER;
           let minRoute = {};
           routes.forEach((route) => {
@@ -230,8 +253,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 departureTime: data.params[which].time
               }
             };
-            directionsService.route(options, (response, status) => {
-              if (status === 'OK') { //// response.routes[{legs[{duration.text, duration.value}]}]
+            methods.getDirectionAsPromise(options)
+              .then((response) => {
                 let leg = findFastestRoute(response.routes);
                 if (leg.duration.value <= data.params[which].max) { /// it's good: update values and copy
                   zips[internalIndex][which] = {
@@ -245,26 +268,30 @@ document.addEventListener('DOMContentLoaded', () => {
                 } else {
                   ///do nothing
                 }
-              } else {
-                /// handle error gracefully
-              }
-              /// if-else-finally
-              data.status[which].resolved++;
-              if (which === "DRIVING") {
-                $driveStatus.text(`${data.status[which].resolved} of ${data.status[which].total}`);
-              } else {
-                $transitStatus.text(`${data.status[which].resolved} of ${data.status[which].total}`);
-              }
-              if (data.status[which].resolved >= zips.length) { /// we've finished them all, so return
-                res(outputZips);
-              }
-            });
+                data.status[which].resolved++;
+                if (which === "DRIVING") {
+                  $driveStatus.text(`${data.status[which].resolved} of ${data.status[which].total}. ${outputZips.length} match.`);
+                } else {
+                  $transitStatus.text(`${data.status[which].resolved} of ${data.status[which].total}. ${outputZips.length} match.`);
+                }
+                if (data.status[which].resolved >= zips.length) { /// we've finished them all, so return
+                  res(outputZips);
+                }
+              })
+              .catch((err) => {
+                if (err.status === google.maps.DirectionsStatus.OVER_QUERY_LIMIT) { // try again?
+                  index--;
+                } else { // not found or zero results
+                  data.status[which].resolved++;
+                }
+                console.warn(err);
+              });
           }));
           index++;
           if (index >= zips.length) { /// we've started them all, so stop sending
             clearInterval(i);
           }
-        }, 250);
+        }, 2000);
       });
     }
   };
